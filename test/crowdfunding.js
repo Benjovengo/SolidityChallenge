d4e834d476adeed6690a37b6ddbde8939589e7c7
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-07 19:19:30 -0300

    test: update test script

--- test/crowdfunding.js
+++ test/crowdfunding.js
@@ -2,42 +2,53 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 const Web3 = require('web3');
 
+// Helper Functions
+const toWei = (n) => {
+  return Web3.utils.toWei(n.toString(), 'ether')
+}
 
-describe('Token', () => {
+const fromWei = (n) => {
+  return Web3.utils.fromWei(n.toString(), 'ether')
+}
 
+
+/* Tests on deployment */
+describe('Deployment', () => {
+
+  let campainGoal
   let challengeToken
+  let crowdfunding
 
   beforeEach(async () => {
     // Setup accounts
-    [ercContract, person1, person2, person3] = await ethers.getSigners()
+    [firstAccount, person1, person2, person3] = await ethers.getSigners()
     
     // Deploy Token - with an total supply of 1,000,000 tokens
     const ChallengeToken = await ethers.getContractFactory('ChallengeToken')
-    challengeToken = await ChallengeToken.deploy(1000000)
-
-    // Deploy Crowdfunding
-    const campainGoal = 10 // Campain Goal - hard-coded
-    const Crowdfunding = await ethers.getContractFactory('Crowdfunding')
-    const crowdfunding = await Crowdfunding.deploy(challengeToken.address, campainGoal)
-
+    challengeToken = await ChallengeToken.deploy(100)
   })
 
-  describe('Deployment', () => {
+  describe('Token Contract', () => {
+
     it('Challenge Token total supply', async () => {
         let result = await challengeToken.totalSupply()
         result = result/(10**18)
-        expect(await result).to.be.equal(1000000) // change to the value passed to the constructor (deployment script)
+        expect(await result).to.be.equal(100) // change to the value passed to the constructor (deployment script)
     })
-    it('Transfer token between accounts', async () => {
-       let amountInWei = 100000
 
-      let amount = Web3.utils.toWei(amountInWei.toString(), 'ether')
-      await challengeToken.transfer(person1.address, amount, { from: ercContract.address })
+    it('Transfer token between accounts', async () => {
+      let amountInWei = 100000
+      let amount = toWei(amountInWei)
+      await challengeToken.transfer(person1.address, amount, { from: firstAccount.address })
 
       let balance = await challengeToken.balanceOf(person1.address)
       balance =  Web3.utils.fromWei(balance.toString(), 'ether')
       expect(balance).to.be.equal(amountInWei.toString())
     })
+
+
+
   })
+
 
 })
