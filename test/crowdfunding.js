385207c97139a01f62c05016e92f348cc1a5a6e6
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-07 19:40:13 -0300

    test: add deployment tests
    Send transactions from one account to the other

--- test/crowdfunding.js
+++ test/crowdfunding.js
@@ -26,29 +26,53 @@ describe('Deployment', () => {
     // Deploy Token - with an total supply of 1,000,000 tokens
     const ChallengeToken = await ethers.getContractFactory('ChallengeToken')
     challengeToken = await ChallengeToken.deploy(100)
+
+    // transfer fund to person1
+    let amountInWei = 5
+    let amount = toWei(amountInWei)
+    let transaction = await challengeToken.connect(firstAccount).approve(person1.address, amount)
+    await transaction.wait()
+    await challengeToken.transfer(person1.address, amount, { from: firstAccount.address })
+
+    // Deploy Crowdfunding
+    campainGoal = 10 // Campain Goal - hard-coded
+    const Crowdfunding = await ethers.getContractFactory('Crowdfunding')
+    crowdfunding = await Crowdfunding.deploy(challengeToken.address, campainGoal)
+
   })
 
   describe('Token Contract', () => {
-
     it('Challenge Token total supply', async () => {
         let result = await challengeToken.totalSupply()
-        result = result/(10**18)
+        result = Number(fromWei(result))
         expect(await result).to.be.equal(100) // change to the value passed to the constructor (deployment script)
     })
 
     it('Transfer token between accounts', async () => {
-      let amountInWei = 100000
-      let amount = toWei(amountInWei)
-      await challengeToken.transfer(person1.address, amount, { from: firstAccount.address })
-
       let balance = await challengeToken.balanceOf(person1.address)
-      balance =  Web3.utils.fromWei(balance.toString(), 'ether')
-      expect(balance).to.be.equal(amountInWei.toString())
+      balance =  fromWei(balance.toString())
+      expect(balance).to.be.equal('5')
     })
 
+    it('Transfer back', async () => {
+      let amountInWei = 5
+      let amount = toWei(amountInWei)
 
+      let transaction = await challengeToken.connect(person1).approve(firstAccount.address, amount)
+      await transaction.wait()
 
+      await challengeToken.transferFrom(person1.address, firstAccount.address, amount)
+
+      let balance = await challengeToken.balanceOf(firstAccount.address)
+      balance =  fromWei(balance.toString())
+      expect(balance).to.be.equal('100')
+    })
   })
 
-
+  describe('Crowdfunding Contract', () => {
+    it('Deployment', async () => {
+        expect(crowdfunding.address).to.be.not.equal('')
+    })
+  })
 })
+
