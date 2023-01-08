d2b3b99328a2db974049f88e28958fe7da690191
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-08 15:06:25 -0300

    build: fix upgradeable version of Crowdfunding contract

--- contracts/Crowdunding.sol
+++ contracts/Crowdunding.sol
@@ -21,21 +21,21 @@ contract Crowdfunding {
     mapping(address => uint256) public pledges; // Pledges from each user
     uint256 public totalPledges; // Total number of different user accounts
     mapping(uint256 => address) public listOfUsers; // List of all users (used to pay back in case of cancelling of contract)
-    bool private newUser = false;
-    bool private goalWasReached = false;
+    bool private newUser; // is a new pledger?
+    bool private goalWasReached; // is the goal reached?
 
     /* Events for DApps to observe changes on state */
     // Changes in states
     event stateChanged(
-        uint256 _totalRaisedDApps,
-        uint256 _totalPledgesDApps,
-        bool _newUserToList,
-        bool _goalReached
+        uint256 totalRaisedDApps,
+        uint256 totalPledgesDApps,
+        bool newUserToList,
+        bool goalReached
     );
 
-    event goalReached(bool _hasReached); // Goal reached
-    event claimFunds(bool _claimedFunds); // Claim funds
-    event cancelCrowdfunding(bool _returnTokensToPledgers); // Return funds to original ownersS
+    event goalReached(bool hasReached); // Goal reached
+    event claimFunds(bool claimedFunds); // Claim funds
+    event cancelCrowdfunding(bool returnTokensToPledgers); // Return funds to original ownersS
     event withdrawFunds(address whoPledged, uint256 amount); // withdraw funds
 
     function initialize(
@@ -79,5 +79,48 @@ contract Crowdfunding {
             goalWasReached = true;
         }
         emit stateChanged(totalRaised, totalPledges, newUser, goalWasReached); // emit event for DApps
+    }
+
+    /* Withdraw Function
+       - msg.sender can only claim up to the amount funded stored on pledges[msg.sender]
+    */
+    function withdraw(uint256 amount) public {
+        require(
+            amount <= pledges[msg.sender],
+            "Amount must be lesser than or equal to the amount funded"
+        );
+        require(totalRaised <= goal, "Goal has been reached"); // if the goal was reached, msg.sender can't have back the funds
+        require(token.transfer(msg.sender, amount), "Transfer failed"); //transfer funds
+        pledges[msg.sender] = pledges[msg.sender] - amount; // resets the amount sent by msg.sender
+        emit withdrawFunds(msg.sender, amount);
+    }
+
+    /* Claim Function
+       - once the goal has been reached, the owner can claim the funds in the contract
+    */
+    function claim() public {
+        require(msg.sender == owner, "Only the owner can cancel the campaign"); // only the owner can call this function
+        require(totalRaised >= goal, "Goal has not yet been reached"); // if the goal was reached, msg.sender can't claim the funds transferred
+        require(totalRaised > 0, "There are no pledged any funds available"); // amount must be positive
+        require(token.transfer(msg.sender, totalRaised), "Transfer failed"); //transfer funds
+        emit claimFunds(true);
+    }
+
+    /* Cancel Function 
+       - refunds all users 
+       - each one will be refunded the amount pledged
+       - only the owner can call this function
+       - the function can't be called if the goal has been reached
+    */
+    function cancel() public {
+        require(msg.sender == owner, "Only the owner can cancel the campaign"); // only the owner can call this function
+        require(totalRaised < goal, "Goal has already been reached"); // asserts that the goal hasn't been reached
+        // refund for all users
+        for (uint256 i = 1; i < totalPledges + 1; i++) {
+            uint256 amount = pledges[listOfUsers[i]]; // listOfUsers[i] is the address for a particular user
+            require(token.transfer(listOfUsers[i], amount), "Transfer failed"); // transfer the funds
+            pledges[listOfUsers[i]] = 0; // if the transfer is successfull, resets the amount funded by the particular address
+        }
+        emit cancelCrowdfunding(false);
     }
 }
