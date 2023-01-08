f5eb37c4ac943b6a15f99905265f580cace98de7
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-07 21:28:31 -0300

    feat: ad claim functionality

--- contracts/Crowdunding.sol
+++ contracts/Crowdunding.sol
@@ -12,7 +12,7 @@ contract Crowdfunding {
 
     // Variables - declarations
     IERC20 private token; // Custom ERC20 token for this campaign
-    address public owner; // Owner of the contract
+    address private owner; // Owner of the contract
     uint256 public goal; // Goal for the campaign
     uint256 public totalRaised; // Total amount raised so far
     mapping(address => uint256) public pledges; // Pledges from each user
@@ -73,15 +73,16 @@ contract Crowdfunding {
     }
 
     /* Claim Function 
-       - once the 
+       - once the goal has been reached, the owner can claim all the funds in the contract
     */
     function claim() public {
         require(msg.sender == owner, "Only the owner can cancel the campaign"); // only the owner can call this function
         require(totalRaised >= goal, "Goal has not yet been reached"); // if the goal was reached, msg.sender can't claim the funds transferred
-        uint256 amount = pledges[msg.sender]; // gets the amount funded by msg.sender
-        require(amount > 0, "You have not pledged any funds"); // amount must be positive
-        require(token.transfer(msg.sender, amount), "Transfer failed"); //transfer funds
-        pledges[msg.sender] = 0; // resets the amount sent by msg.sender
+        require(totalRaised > 0, "You have not pledged any funds"); // amount must be positive
+        require(
+            token.transferFrom(address(this), msg.sender, totalRaised),
+            "Transfer failed"
+        ); //transfer funds
     }
 
     /* Cancel Function 
