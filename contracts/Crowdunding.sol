f0724965f8c3c4d880a60c9018568318e6c975e3
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-08 12:32:53 -0300

    build: import upgradeable modules

--- contracts/Crowdunding.sol
+++ contracts/Crowdunding.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-// Import Modules
+/** Imports **/
+// Safe modules
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+// Upgradeable modules
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/upgrades/contracts/upgradeability/Proxy.sol";
 
 contract Crowdfunding {
     // Safe definitions - OpenZeppelin
