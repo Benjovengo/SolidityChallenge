d51748107b384aa70be57cdf316e211d816bcf0e
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-08 06:00:39 -0300

    build: working on localhost deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,24 +6,49 @@
 // global scope, and execute the script.
 const { ethers } = require("hardhat");
 const hre = require("hardhat");
+const Web3 = require('web3');
+
+// Helper Functions
+const toWei = (n) => {
+  return Web3.utils.toWei(n.toString(), 'ether')
+}
 
 async function main() {
+    // Setup accounts
+    [firstAccount, person1, person2, person3] = await ethers.getSigners()
+    
   // Deploy Token - with an total supply of 1,000,000 tokens
   const ChallengeToken = await ethers.getContractFactory('ChallengeToken')
   const challengeToken = await ChallengeToken.deploy(1000000)
   await challengeToken.deployed()
-
   console.log(`Deployed Challenge Token Contract at: ${challengeToken.address}`)
-  console.log(`Total supply: ${challengeToken.totalSupply()} tokens\n`)
+  console.log(`Total supply: ${await challengeToken.totalSupply()} tokens\n`)
 
   // Deploy Crowdfunding
   const campainGoal = 10 // Campain Goal - hard-coded
   const Crowdfunding = await ethers.getContractFactory('Crowdfunding')
   const crowdfunding = await Crowdfunding.deploy(challengeToken.address, campainGoal)
   await crowdfunding.deployed()
-
   console.log(`Deployed Crowdfunding Contract at: ${crowdfunding.address}`)
-  console.log(`Total supply: ${challengeToken.totalSupply()} tokens\n`)
+
+    // Transfer tokens to accounts so all accounts have tokens
+    let amountInWei = 100000 // value in CHAL
+    let amount = toWei(amountInWei)
+
+    // transfer fund to person1
+    let transaction = await challengeToken.connect(firstAccount).approve(person1.address, amount)
+    await transaction.wait()
+    await challengeToken.transfer(person1.address, amount, { from: firstAccount.address })
+
+    // transfer fund to person2
+    transaction = await challengeToken.connect(firstAccount).approve(person2.address, amount)
+    await transaction.wait()
+    await challengeToken.transfer(person2.address, amount, { from: firstAccount.address })
+
+    // transfer fund to person3
+    transaction = await challengeToken.connect(firstAccount).approve(person3.address, amount)
+    await transaction.wait()
+    await challengeToken.transfer(person3.address, amount, { from: firstAccount.address })
 }
 
 // We recommend this pattern to be able to use async/await everywhere
