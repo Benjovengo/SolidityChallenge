f4d8bc71338707b844ef6f8c57edafda3d658a8a
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-08 14:29:58 -0300

    build: new upgradeable token

--- contracts/ChallengeToken.sol
+++ contracts/ChallengeToken.sol
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-// Import OpenZeppelin Modules
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-// Imports for security checked functions
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+/** Imports **/
+// Upgradeable modules
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract ChallengeToken is ERC20, Ownable {
-    // using functions with security checks
-    using SafeMath for uint256;
-    using SafeERC20 for IERC20;
+contract ChallengeToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
 
-    // Initialize with the total supply for the token
-    constructor(uint256 totalTokens) ERC20("Challenge", "CHAL") {
-        _mint(msg.sender, totalTokens * 10**decimals());
+    function initialize(uint256 _totalSupply) public initializer {
+        __ERC20_init("ChallengeToken", "CHAL");
+        __Ownable_init();
+        mint(msg.sender, _totalSupply * 10**decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
